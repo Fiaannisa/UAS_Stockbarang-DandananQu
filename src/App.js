130ea5e332f5147a685f01e4746da6ec9555cdb6
fiaannisa <annisafia06@gmail.com>
2020-06-13 10:21:29 +0700

    Routing di React.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
+import {BrowserRouter, Route, Switch } from 'react-router-dom'
 import logo from './logo.svg';
 import Home from './Component/Fungsional/Home';
 import Parent from './Component/Class/Parent';
 import BootstrapComp from './Component/Class/BootstrapComp';
 import NavbarComp from './Component/Fungsional/NavbarComp';
+import AboutComp from './Component/Fungsional/AboutComp';
+import HomePage from './Component/Fungsional/HomePage';
+import About from './Component/Fungsional/AboutComp';
+import { Form } from 'reactstrap';
 //import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <NavbarComp/>
-    </div>
+    <BrowserRouter>
+      <NavbarComp />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/about" component={About} />
+      </Switch>
+    </BrowserRouter>
+
+
   );
 }
 
