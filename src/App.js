a4fbe6906d9259fd4be89a61c6255d73ad20dc4a
fiaannisa <annisafia06@gmail.com>
2020-06-13 10:05:41 +0700

    Package NavbarCom.jsx

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import logo from './logo.svg';
 import Home from './Component/Fungsional/Home';
 import Parent from './Component/Class/Parent';
 import BootstrapComp from './Component/Class/BootstrapComp';
+import NavbarComp from './Component/Fungsional/NavbarComp';
 //import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <BootstrapComp/>
+      <NavbarComp/>
     </div>
   );
 }
