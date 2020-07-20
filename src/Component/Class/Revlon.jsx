6b242705b052d1a99e807f5dcfff028a043b0852
fiaannisa <annisafia06@gmail.com>
2020-07-20 13:15:54 +0700

    tampil berdasarkan merk-revlon

--- src/Component/Class/Revlon.jsx
+++ src/Component/Class/Revlon.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom'
 import './CSS/barang.css';
 
 const api = 'http://localhost:3001'
-
+//tampil berdasarkan merk-revlon
 class Revlon extends PureComponent {
     constructor(props) {
         super(props)
