f6f55fc8e2e57391d5d90afb71f04e495dc5d6f6
fiaannisa <annisafia06@gmail.com>
2020-07-20 12:29:19 +0700

    Web Stock Barang

--- src/Component/Fungsional/FormLogin.jsx
+++ src/Component/Fungsional/FormLogin.jsx
@@ -3,7 +3,7 @@ import { Button, Container, Col, Alert, Row, Form, FormGroup, Label, Input, Card
 import './CSS/Blog.css';
 import username from '../Fungsional/tentang';
 
-//Form Login
+//Form Login Web
 
 class FormLogin extends PureComponent {
     constructor(props) {
